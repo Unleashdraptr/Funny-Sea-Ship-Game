621ccd015d7f6722001e86ba980631a8e5979218
Unleashdraptr <98326571+Unleashdraptr@users.noreply.github.com>
2023-08-21 17:59:29 +0100

    Unreal Setup
    Added The project Folder now to fuck up C++ :)

---
+++ Source/OceanTravellers/OceanTravellers.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OceanTravellers.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OceanTravellers, "OceanTravellers" );
