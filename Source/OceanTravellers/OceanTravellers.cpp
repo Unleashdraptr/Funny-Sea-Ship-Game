7251d2b9c9a6ad8acb0e5491b82883b3db7318bd
Unleashdraptr <98326571+Unleashdraptr@users.noreply.github.com>
2023-08-22 15:58:47 +0100

    Revert "Unreal Setup"
    This reverts commit 621ccd015d7f6722001e86ba980631a8e5979218.

--- Source/OceanTravellers/OceanTravellers.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "OceanTravellers.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OceanTravellers, "OceanTravellers" );
